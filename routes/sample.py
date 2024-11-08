ff7c3e7813296d4b7bc853f634db1833f25d1766
Sarthak Borade <sarthakb009@gmail.com>
2024-11-09 00:48:26 +0530

    Fixed bug in user authentication module new

--- routes/sample.py
+++ routes/sample.py
@@ -9,3 +9,11 @@ sample_bp = Blueprint('sample', __name__)
 @login_required
 def protected():
     return jsonify({'message': f'Hello, {current_user.username}! This is a protected route.'}), 200
+
+
+@sample_bp.route('/test')
+def test():
+    return jsonify({
+        'message': 'Server is working!',
+        'status': 'success'
+    }), 200
